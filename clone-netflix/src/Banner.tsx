69de7c09e3f9ad797aaea0c17fe7d4f55f6e5948
hamadayuuki <yuukirinnma@gmail.com>
2023-06-18 05:51:54 +0900

    add: fetch banner data

--- clone-netflix/src/Banner.tsx
+++ clone-netflix/src/Banner.tsx
@@ -9,3 +9,29 @@ type Banner = {
     backdrop_path?: string;
     overview?: string;
 };
+
+export const Banner = () => {
+    const [banner, setBanner] = useState<Banner>({});
+
+    useEffect( () => {
+        async function fetchData() {
+            const response = await axios.get(requests.feachNetflixOriginals);
+            console.log(response.data.result)
+            const index = Math.floor(Math.random() * response.data.results.length - 1);
+            setBanner(response.data.results[index]);
+            return response;
+        };
+        
+        fetchData();
+    }, []);
+
+    console.log(banner);
+
+    // descriptionの長さを調整する
+    function truncate(str: any, n: number) {
+        // undefinedを弾く
+        if (str !== undefined) {
+        return str.length > n ? str?.substr(0, n - 1) + "..." : str;
+        }
+    }
+}
