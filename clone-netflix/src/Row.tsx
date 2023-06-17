2532d3050810485d16cbd7c7e1e86bd4cf12162f
hamadayuuki <yuukirinnma@gmail.com>
2023-06-18 05:02:21 +0900

    add: show movie cards

--- clone-netflix/src/Row.tsx
+++ clone-netflix/src/Row.tsx
@@ -1,6 +1,9 @@
 /* 映画カードのデータ */
-import React, { useState, useEffect } from "react"
-import axios from "./axios"
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.scss";
+
+const img_base_url = "https://image.tmdb.org/t/p/original";
 
 type Props = {
     title: string;
@@ -17,7 +20,7 @@ type Movie = {
     backdrop_path: string;
 };
 
-export const Row = ({title, fetchUrl}: Props) => {
+export const Row = ({title, fetchUrl, isLargeRow}: Props) => {
     // 状態を通知する
     //     ↓変数  ↓変更を反映させるメソッド名
     const [movies, setMovies] = useState<Movie[]>([]);
@@ -35,6 +38,18 @@ export const Row = ({title, fetchUrl}: Props) => {
     console.log(movies);
 
     return(
-        <div className = "Row " />
+        <div className="Row">
+            <h2>{title}</h2>
+            <div className="Row-posters">
+            {movies.map((movie, i) => (
+                <img
+                    key={movie.id}
+                    className={`Row-poster ${isLargeRow && "Row-poster-large"}`}
+                    src={`${img_base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                    alt={movie.name}
+                />
+            ))}
+            </div>
+        </div>
     )
 };
