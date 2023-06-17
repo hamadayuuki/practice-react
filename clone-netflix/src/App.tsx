6f03c06e28e77de0d00efd05025216150ccc3cfc
hamadayuuki <yuukirinnma@gmail.com>
2023-06-18 05:03:15 +0900

    clean: unused code commentout

--- clone-netflix/src/App.tsx
+++ clone-netflix/src/App.tsx
@@ -14,9 +14,9 @@ function App() {
           isLargeRow
         />
         <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
-        <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+        {/* <Row title="Action Movies" fetchUrl={requests.feactActionMovies} /> */}
         <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
-        <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+        {/* <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} /> */}
         <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
         <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
     </div>
