c22d212b502b6fc3d36d03caf5ed56d3f0483185
hamadayuuki <yuukirinnma@gmail.com>
2023-06-18 05:52:38 +0900

    add: banner DOM

--- clone-netflix/src/Banner.tsx
+++ clone-netflix/src/Banner.tsx
@@ -34,4 +34,30 @@ export const Banner = () => {
         return str.length > n ? str?.substr(0, n - 1) + "..." : str;
         }
     }
+
+    // DOM
+    return(
+        <header
+        className = "Banner"
+        style = {{
+            backgroundSize: "cover",
+            backgroundImage: `url("https://image.tmdb.org/t/p/original${banner?.backdrop_path}")`,
+            backgroundPosition: "center center",
+        }}
+       >
+            <div className="Banner-contents">
+                <h1 className="banner-title">
+                {banner?.title || banner?.name || banner?.original_name}
+                </h1>
+
+                <div className="Banner-buttons">
+                    <button className="Banner-button">Play</button>
+                    <button className="Banner-button">My List</button>
+                </div>
+
+                <h1 className="Banner-description">{truncate(banner?.overview, 150)}</h1>
+            </div>
+            <div className="Banner-fadeBottom" />
+        </header>
+    );
 }
