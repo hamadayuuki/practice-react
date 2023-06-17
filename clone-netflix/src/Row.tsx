1fab86ddd4d9a29b63b7bc5e5e25a7184b34970e
hamadayuuki <yuukirinnma@gmail.com>
2023-06-18 04:07:48 +0900

    add: movie entity(Props+Movie)

---
+++ clone-netflix/src/Row.tsx
@@ -0,0 +1,17 @@
+/* 映画カードのデータ */
+import React from "react"
+
+type Props = {
+    title: string;
+    fetchUrl: string;
+    isLargeRow?: boolean;
+};
+
+type Movie = {
+    id: string;
+    name: string;
+    title: string;
+    original_name: string;
+    poster_path: string;
+    backdrop_path: string;
+};
